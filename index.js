f5270595df1bc3ed3bdd93a894f528ccf471a820
Anoop <anooprav7@gmail.com>
2020-04-24 04:06:54 +0530

    stable release

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
+
+"use strict"
+
 const fs = require("fs-extra")
 const path = require("path")
 const { spawnSync, spawn } = require("child_process")
 const os = require("os")
-const { generatePackageJson } = require("./config")
+const { html, css, generatePackageJson } = require("./config")
 
 let folderName
 let root
@@ -10,7 +14,7 @@ let root
 function addDependencies() {
   let dependencies = ["lite-server@2.5.4"]
   let command = "yarnpkg"
-  args = ["add", ...dependencies, "--exact", "--save-dev"]
+  let args = ["add", ...dependencies, "--exact", "--save-dev"]
   args.push("--cwd")
   args.push(root)
 
@@ -20,15 +24,22 @@ function addDependencies() {
 function addTemplateFiles() {
   // package.json
   const packageJson = generatePackageJson(folderName)
-  const templateRoot = path.resolve("template-files")
+  // const templateRoot = path.resolve("template-files")
+  // const templateRoot = require.resolve("./template-files/index.html")
 
   fs.writeFileSync(
     path.join(root, "package.json"),
     JSON.stringify(packageJson, null, 2) + os.EOL
   )
 
+  fs.writeFileSync(path.join(root, "index.html"), html + os.EOL)
+  fs.writeFileSync(path.join(root, "normalize.css"), css + os.EOL)
+  fs.writeFileSync(path.join(root, "styles.css"), "" + os.EOL)
+  fs.writeFileSync(path.join(root, "index.js"), "" + os.EOL)
+  fs.writeFileSync(path.join(root, ".gitignore"), "node_modules/" + os.EOL)
+
   // copy template files
-  fs.copySync(templateRoot, root)
+  // fs.copySync(templateRoot, root)
 }
 
 function init() {
